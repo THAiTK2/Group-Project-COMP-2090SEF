c52bd496d6b9a559ec57ef9994bc224fce62bb5a
TkWong <weithaiw@gmail.com>
2026-03-05 13:13:14 +0800

    Create code_matrix.py

---
+++ task2/code_matrix.py
@@ -0,0 +1,23 @@
+# Creat a matrix :
+  matrix_name = [
+    [n1, n2,..],            # method_1
+    [.....], 
+    [.....]
+]
+# the number of rows and columns are determined by user 
+
+
+import numpy                 # import numpy to operate numerical data and scientific computing
+
+A = numpy.array([[1, 2], [3, 4]]) # method_2
+B = numpy.array([[5, 6], [7, 8]]) 
+
+-----------------------------------------
+
+# Matrix operations
+print(A + B)   # Matrix addition 
+
+ # Matrix multiplication
+print(A @ B)   OR  print(np.matmul(A,B)) 
+
+print(A.T)     # Transpose. Element in matrix A will be re-arranged order by column. 
